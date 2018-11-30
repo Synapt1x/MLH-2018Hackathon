7dea5fee8ff07bae34077ccb3eb55b8e3e19cb7b
synapt1x <chriscadonic@gmail.com>
2018-11-30 12:20:16 -0600

    Update requirements; update loading and check bgsub

--- main.py
+++ main.py
@@ -35,11 +35,20 @@ def main():
     with open(yaml_file, "r") as f:
         config = yaml.load(f)
 
+    # extract list of videos from data dir
     vid_dir = os.path.join(curdir, config['traindir'])
-
     vid_names = util.load_data(vid_dir)
 
-    #TODO: finish rest
+    # extract background subtraction image from bg vid
+    bg_file = os.path.join(curdir, config['bg_img'])
+    bg_image = util.extract_bg_image(bg_file)
+
+    #TODO: finish rest and add GUI
+    clicked = True  # To be replaced by gui selection to start
+    if clicked:
+        util.process_video(vid_names[0], bg_image)  # just the first vid for now
+
+    print("vid names:", vid_names)
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -35,6 +35,66 @@ def load_data(dir_name=None):
     return output_files
 
 
+def load_video(vid_name):
+    """
+    Load new video and return cv2 Video object and initial frame.
+
+    :param vid_name:
+    :return:
+    """
+
+    # init output vars
+    valid = False
+    # noinspection PyArgumentList
+    video = cv2.VideoCapture(vid_name)
+    init_frame = np.array([], dtype=np.uint8)
+
+    # try load video and print out any issues
+    if video is None or not video.isOpened():
+        print("Video not found!")
+    else:
+        valid, init_frame = video.read()
+
+        if valid is False or valid is None:
+            print("Error extracting frame from video!")
+
+    return valid, video, init_frame
+
+
+def extract_bg_image(vid_name):
+    """
+    Extract background image from background video.
+
+    :param vid_name:
+    :return:
+    """
+
+    valid, video, init_frame = load_video(vid_name)
+
+    # TODO: Just to ensure it's working properly
+    cv2.imshow('background image', init_frame)
+    cv2.waitKey(0)
+
+    if valid:
+        return init_frame
+
+
+def process_video(vid_name, bg_frame):
+    """
+    Parse video for the relevant motion/lack-of-motion detection.
+
+    :param dir_name:
+    :return:
+    """
+
+    valid, video, frame = load_video(vid_name)
+
+    while valid:
+        diff_frame = background_subtraction(frame, bg_frame, thresh=0.2)
+
+        #TODO: Finish
+
+
 def background_subtraction(frame, bg_frame, thresh=0.2):
     """
     Uses background subtraction to subtract out stationarity in frame diffs.
@@ -44,18 +104,22 @@ def background_subtraction(frame, bg_frame, thresh=0.2):
     :return:
     """
 
-    #TODO: Finish/Verify
-    diff = np.zeros(shape=frame.shape)
+    #TODO: May not work without exact bg frame
+
+    mask = np.zeros(shape=frame.shape[:2], dtype=np.uint8)
+    img = cv2.cvtColor(frame.copy(), cv2.COLOR_RGB2GRAY)
+    bg_img = cv2.cvtColor(bg_frame, cv2.COLOR_RGB2GRAY)
+
     if frame.shape == bg_frame.shape:
-        diff = (frame.astype / 255.) - (bg_frame / 255.)
+        diff = np.abs((img / 255.) - (bg_img / 255.))
     else:
         # create mask based on thresholded correlation between frames
-        diff = cv2.filter2D(frame, ddepth=-1, kernel=bg_frame)
+        diff = cv2.filter2D(img, ddepth=-1, kernel=bg_img)
 
-    diff[diff > thresh] = 0
-    diff[diff <= thresh] = 1
+    mask[diff > thresh] = 0
+    mask[diff <= thresh] = 1
 
-    frame = cv2.bitwise_and(frame, frame, mask=diff)
+    frame = cv2.bitwise_and(frame, frame, mask=mask)
 
     return frame
 
